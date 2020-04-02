83dc43eb8f069c1d6c975b31532c7ca8ef41bc72
Bryan Garro Eduarte <bryangarroeduarte@hotmail.com>
2020-04-01 20:28:18 -0600

    cursos

--- app/src/main/java/com/example/lab2_moviles/AccesoDatos/Data.java
+++ app/src/main/java/com/example/lab2_moviles/AccesoDatos/Data.java
@@ -1,12 +1,13 @@
 package com.example.lab2_moviles.AccesoDatos;
 
 import com.example.lab2_moviles.LogicaNegocio.Profesor;
-
+import com.example.lab2_moviles.LogicaNegocio.Curso;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Data {
     private List<Profesor> profesorList;
+    private List<Curso> cursoList;
     public Data() {
         profesorList = new ArrayList<>();
         this.prepareProfesorData();
@@ -24,10 +25,33 @@ public class Data {
         profesor = new Profesor("456", "Jesus", "@Jesus", 978);
         profesorList.add(profesor);
     }
+    public void prepareCursoData() {
+        Curso curso = new Curso("ST", "Soporte", 3, 4);
+        cursoList.add(curso);
+        curso = new Curso("FD", "Fundamentos", 3, 4);
+        cursoList.add(curso);
+        curso = new Curso("PG1", "Programacion I", 3, 4);
+        cursoList.add(curso);
+        curso = new Curso("PG2", "Programacion II", 3, 4);
+        cursoList.add(curso);
+        curso = new Curso("PG3", "Programacion III", 3, 4);
+        cursoList.add(curso);
+        curso = new Curso("PG4", "Programacion IV", 3, 4);
+        cursoList.add(curso);
+        curso = new Curso("EDA", "Estructuras Datos", 3, 4);
+        cursoList.add(curso);
+        curso = new Curso("EDI", "Estructuras Discretas", 3, 4);
+        cursoList.add(curso);}
     public List<Profesor> getProfesorList() {
         return profesorList;
     }
     public void setProfesorList(List<Profesor> profesorList) {
         this.profesorList = profesorList;
+    }
+    public List<Curso> getCursoList() {
+        return cursoList;
+    }
+    public void setCursoList(List<Curso> cursoList) {
+        this.cursoList = cursoList;
     }
 }
